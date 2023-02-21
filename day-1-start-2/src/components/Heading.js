e20bd732ada27a51d95674752494babea68bad31
Michael Wasilewski <michael_grimestad94@hotmail.com>
2023-02-21 14:04:13 +0100

    Setup CRA , make components

---
+++ day-1-start-2/src/components/Heading.js
@@ -0,0 +1,8 @@
+function heading(){
+    return (
+        
+        <h1>Sander</h1>
+        
+    )
+}
+export default heading;
---
+++ day-1-start-2/src/components/List.js
@@ -0,0 +1,15 @@
+function List() {
+    return (
+        (
+            <div>
+                <ul>
+                    <li>Travel</li>
+                    <li>Football</li>
+                    <li>Histrory</li>
+                </ul>
+            </div>
+        )
+    )
+  
+}
+export default List;
